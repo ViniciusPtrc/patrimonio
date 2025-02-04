2f9b7b91f52f263f94dbfada1eae3d6dc48092a2
ViniciusPatricio <viniciuspatricio.adm@gmail.com>
2025-02-03 23:37:57 -0300

    Corrigindo porta e host para Replit

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def index():
     return render_template("index.html", resultados=resultados)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
